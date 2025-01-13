775091fd8ae53d94fd72ef9475c3f68158d19863
VEdant732 <VedantAjay377@gmail.com>
2025-01-13 20:32:18 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var time=new Date().getDay();
+    console.log(time)
+    if (time<12){
+        comnsole.log("good morning vedant")
+    }
+    else if(time>14){
+        console.log("good Afternoon Vedant")
+}
+    else{
+        console.log("good evening Vedant")
+    }
